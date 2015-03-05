df89d44a657a700e03fea69e2f1bdfa5ff202967
cp <cesar.passoni@gmail.com>
2015-03-05 06:08:56 -0500

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+mvn clean install
+scp -i ~/.ssh/asdf.pem  target/AppTest-1.0-SNAPSHOT-exec.jar ec2-user@54.88.144.125:~/
+ssh -i ~/.ssh/asdf.pem   ec2-user@54.88.144.125 rdeploy.sh
+cat rdeploy.sh | ssh -i ~/.ssh/asdf.pem   ec2-user@54.88.144.125 $1
