3651b50ce4cd5ca57b2e7990d894348c4ef2e69e
cp <cesar.passoni@gmail.com>
2015-03-09 21:26:07 -0400

    Clean code

--- src/main/java/ca/app/frontEnd/controller/ConverterController.java
+++ src/main/java/ca/app/frontEnd/controller/ConverterController.java
@@ -1,8 +1,6 @@
 package ca.app.frontEnd.controller;
 
 
-import ca.app.user.service.ISVService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -12,17 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ConverterController {
 
-
-    @Autowired
-    private ISVService isvService;
-
-
     @PreAuthorize("hasRole('USER')")
     @RequestMapping("/converter")
     public String converter(Model model) {
         model.addAttribute("principal", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
         return "converter";
     }
 }
